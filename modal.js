7c8a2812d99a9d2e82f322a1be082a9929ae0060
MarikaEvance <99250700+MarikaEvance@users.noreply.github.com>
2022-08-13 10:45:08 +0300

    открытие/закрытие модалки

--- modal.js
+++ modal.js
@@ -10,3 +10,11 @@ modalClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.remove("modal-show");
 });
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modalPopup.classList.remove("modal-show");
+    }
+  }
+});
